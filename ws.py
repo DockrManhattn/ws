8ec9b4a45d8c98cffe8f0ba0e2b4f5fc56e05392
DockrManhattn <38168619+DockrManhattn@users.noreply.github.com>
2025-01-26 20:17:23 -0500

    Create ws.py

---
+++ ws.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import sys
+import socket
+
+
+blue = "\033[94m"
+reset = "\033[0m"
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
+
+def start_web_server(port=80):
+    if is_port_in_use(port):
+        print(f"{blue}There is already a web server running on port {port}.{reset}")
+        sys.exit(1)
+
+    command = f"sudo python3 -m http.server {port}"
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except subprocess.CalledProcessError as e:
+        print(f"Error starting the web server: {e}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description="Start a web server.")
+    parser.add_argument("port", nargs="?", type=int, help="Port number for the web server")
+
+    args = parser.parse_args()
+
+    if args.port is None:
+        start_web_server()
+    else:
+        start_web_server(args.port)
+
+if __name__ == "__main__":
+    main()
