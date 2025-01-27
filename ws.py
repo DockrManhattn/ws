ab63d85dab5efc312361f14ee6ce147953d31c4e
DockrManhattn <38168619+DockrManhattn@users.noreply.github.com>
2025-01-26 22:13:04 -0500

    Update ws.py

--- ws.py
+++ ws.py
@@ -5,26 +5,26 @@ import sys
 import socket
 
 
+blue = "\033[94m"
+reset = "\033[0m"
+
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', port)) == 0
 
 def start_web_server(port=80):
     if is_port_in_use(port):
-        print(f"There is already a web server running on port {port}.")
+        print(f"{blue}There is already a web server running on port {port}.{reset}")
         sys.exit(1)
 
     command = f"sudo python3 -m http.server {port}"
     try:
         subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        if e.returncode == 1 and "Address already in use" in str(e):
-            print(f"There is already a web server running on port {port}.")
-        else:
-            print(f"Error starting the web server: {e}")
-        sys.exit(1)
     except KeyboardInterrupt:
         sys.exit(0)
+    except subprocess.CalledProcessError as e:
+        print(f"Error starting the web server: {e}")
+        sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="Start a web server.")
@@ -41,4 +41,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        pass
+        print("")
